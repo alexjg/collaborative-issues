06cd7857d2fb4175d225b37285c421eca4116fc3
Alex Good <alex@memoryandthought.me>
2022-04-12 10:57:15 +0100

    update to new non-authorized cobs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use anyhow::anyhow;
 use argh::FromArgs;
 use lazy_static::lazy_static;
 use librad::{
     collaborative_objects::{History, NewObjectSpec, ObjectId, TypeName},
     git::{identities::local, storage::Storage, Urn},
-    PublicKey, SecretKey,
     profile::Profile,
-    crypto::{BoxedSigner, SomeSigner},
-    keystore::{
-        crypto::{self, Pwhash},
-        pinentry::Prompt,
-        FileStorage, Keystore,
-    }
 };
 use std::{
+    collections::HashMap,
     convert::{TryFrom, TryInto},
-    fs,
-    path::{Path, PathBuf},
     str::FromStr,
+    io::Write,
 };
 
 /// Interact with radicle collaborative objects
@@ -38,6 +30,7 @@ enum Command {
     AddComment(AddComment),
     List(List),
     ChangeGraph(ChangeGraph),
+    AutomergeDoc(AutomergeDoc),
 }
 
 /// Create a new issue
@@ -87,10 +80,19 @@ struct ChangeGraph {
     issue_id: ObjectId,
 }
 
+/// Dump the validated automerge document
+#[derive(FromArgs, Debug, PartialEq)]
+#[argh(subcommand, name = "automerge-doc")]
+struct AutomergeDoc {
+    /// the ID of the issue
+    #[argh(option)]
+    issue_id: ObjectId,
+}
+
 const SCHEMA_JSON_BYTES: &[u8; 702] = std::include_bytes!("./schema.json");
 
 lazy_static! {
-    static ref TYPENAME: TypeName = FromStr::from_str("xyz.radicle.issue").unwrap();
+    static ref TYPENAME: TypeName = FromStr::from_str("xyz.example.radicle.issue").unwrap();
     static ref SCHEMA_JSON: serde_json::Value =
         serde_json::from_slice(&SCHEMA_JSON_BYTES[..]).unwrap();
 }
@@ -99,10 +101,18 @@ fn main() {
     tracing_subscriber::fmt::init();
     let args: Args = argh::from_env();
     let profile = Profile::load().unwrap();
+    let sock = lnk_clib::keys::ssh::SshAuthSock::default();
+    let signer = match lnk_clib::keys::ssh::signer(&profile, sock) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("failed to load signer: {}", e);
+            return;
+        }
+    };
     let paths = profile.paths();
-    let signer = get_signer(paths.keys_dir()).unwrap();
     let storage = Storage::open(&paths, signer.clone()).unwrap();
     let local_id = local::default(&storage).unwrap().unwrap();
+
 
     match args.command {
         Command::Create(Create { title, description }) => {
@@ -127,15 +137,16 @@ fn main() {
                 .retrieve(&args.project_urn, &TYPENAME, &issue_id)
                 .unwrap();
             if let Some(object) = object {
-                match object.history() {
-                    History::Automerge(bytes) => {
-                        let backend = automerge::Backend::load(bytes.clone()).unwrap();
-                        let mut frontend = automerge::Frontend::new();
-                        frontend.apply_patch(backend.get_patch().unwrap()).unwrap();
+                match evaluate_history(object.history()) {
+                    Ok((mut frontend, _backend)) => {
                         println!(
                             "{}",
                             serde_json::to_string(&frontend.state().to_json()).unwrap()
                         );
+                    },
+                    Err(e) => {
+                        eprintln!("error evaluating {}", e);
+                        return;
                     }
                 }
             } else {
@@ -148,52 +159,59 @@ fn main() {
                 .retrieve(&args.project_urn, &TYPENAME, &issue_id)
                 .unwrap();
             if let Some(object) = object {
-                match object.history() {
-                    History::Automerge(bytes) => {
-                        let mut backend = automerge::Backend::load(bytes.clone()).unwrap();
-                        let mut frontend = automerge::Frontend::new();
-                        frontend.apply_patch(backend.get_patch().unwrap()).unwrap();
-                        let change = frontend
-                            .change(Some("Add a comment".to_string()), |d| {
-                                let comments =
-                                    d.value_at_path(&automerge::Path::root().key("comments"));
-                                let num_comments =
-                                    if let Some(automerge::Value::List(comments)) = comments {
-                                        comments.len() as u32
-                                    } else {
-                                        println!("invalid issue document");
-                                        return Ok(());
-                                    };
-                                let author = local_id.urn();
-                                let new_comment = serde_json::json!({
-                                    "author": author.to_string(),
-                                    "comment": comment,
-                                });
-                                d.add_change(automerge::LocalChange::insert(
-                                    automerge::Path::root().key("comments").index(num_comments),
-                                    automerge::Value::from_json(&new_comment),
-                                ))
-                            })
-                            .unwrap()
-                            .1
-                            .unwrap();
-                        let change: automerge::Change = change.into();
-                        backend.apply_changes(vec![change.clone()]).unwrap();
-                        store
-                            .update(
-                                &local_id,
-                                &args.project_urn,
-                                librad::collaborative_objects::UpdateObjectSpec{
-                                    typename: TYPENAME.clone(),
-                                    object_id: issue_id,
-                                    changes: History::Automerge(change.raw_bytes().to_vec()),
-                                    message: Some("add comment".to_string()),
-                                }
-                            )
-                            .unwrap();
-                        println!("Update complete");
+                let (mut frontend, mut backend) = match evaluate_history(object.history()) {
+                    Ok(it) => it,
+                    Err(e) => {
+                        eprintln!("error loading issue: {}", e);
+                        return;
                     }
-                }
+                };
+                frontend.apply_patch(backend.get_patch().unwrap()).unwrap();
+                let change = frontend
+                    .change(Some("Add a comment".to_string()), |d| {
+                        let comments =
+                            d.value_at_path(&automerge::Path::root().key("comments"));
+                        let num_comments =
+                            if let Some(automerge::Value::List(comments)) = comments {
+                                comments.len() as u32
+                            } else {
+                                eprintln!("invalid issue document");
+                                return Ok(());
+                            };
+                        let author = local_id.urn();
+                        let mut comment_map = HashMap::new();
+                        comment_map.insert("author".into(), automerge::Value::Primitive(automerge::Primitive::Str(author.to_string().into())));
+                        comment_map.insert("comment".into(), automerge::Value::Primitive(automerge::Primitive::Str(comment.into())));
+                        let new_comment = automerge::Value::Map(comment_map);
+                            
+                        //let new_comment = serde_json::json!({
+                            //"author": author.to_string(),
+                            //"comment": comment,
+                        //});
+                        d.add_change(automerge::LocalChange::insert(
+                            automerge::Path::root().key("comments").index(num_comments),
+                            new_comment,
+                        ))
+                    })
+                    .unwrap()
+                    .1
+                    .unwrap();
+                let change: automerge::Change = change.into();
+                backend.apply_changes(vec![change.clone()]).unwrap();
+                let contents = librad::collaborative_objects::EntryContents::Automerge(change.raw_bytes().to_vec());
+                store
+                    .update(
+                        &local_id,
+                        &args.project_urn,
+                        librad::collaborative_objects::UpdateObjectSpec{
+                            typename: TYPENAME.clone(),
+                            object_id: issue_id,
+                            changes: contents,
+                            message: Some("add comment".to_string()),
+                        }
+                    )
+                    .unwrap();
+                println!("Update complete");
             } else {
                 println!("No object found");
             }
@@ -226,41 +244,29 @@ fn main() {
                     .dotviz
             )
         }
-    }
-}
-
-fn get_signer(keys_dir: &Path) -> anyhow::Result<BoxedSigner> {
-    let file = default_signer_file(keys_dir)?;
-    let keystore = FileStorage::<_, PublicKey, _, _>::new(
-        &file,
-        Pwhash::new(
-            Prompt::new("please enter your Radicle password: "),
-            *crypto::KDF_PARAMS_TEST,
-        ),
-    );
-    let key: SecretKey = keystore.get_key().map(|keypair| keypair.secret_key)?;
-
-    Ok(SomeSigner { signer: key }.into())
-}
-
-fn default_signer_file(keys_dir: &Path) -> anyhow::Result<PathBuf> {
-    let mut keys = fs::read_dir(keys_dir)?;
-    match keys.next() {
-        None => Err(anyhow!(
-            "No key was found in `{}`, have you initialised your key yet?",
-            keys_dir.display()
-        )),
-        Some(key) => {
-            if keys.next().is_some() {
-                Err(anyhow!("Multiple keys were found in `{}`, you will have to specify which key you are using", keys_dir.display()))
+        Command::AutomergeDoc(AutomergeDoc { issue_id }) => {
+            let store = storage.collaborative_objects(Some(paths.cob_cache_dir().to_path_buf()));
+            let object = store
+                .retrieve(&args.project_urn, &TYPENAME, &issue_id)
+                .unwrap();
+            if let Some(object) = object {
+                match evaluate_history(object.history()) {
+                    Ok((_frontend, backend)) => {
+                        let mut stdout = std::io::stdout();
+                        stdout.write(backend.save().unwrap().as_slice()).unwrap();
+                    },
+                    Err(e) => {
+                        eprintln!("error: {}", e);
+                    }
+                }
             } else {
-                Ok(key?.path())
+                println!("No object found");
             }
         }
     }
 }
 
-fn initial_doc(author: Urn, title: String, description: String) -> History {
+fn initial_doc(author: Urn, title: String, description: String) -> librad::collaborative_objects::EntryContents {
     let mut frontend = automerge::Frontend::new();
     let (_, change) = frontend
         .change::<_, (), automerge::InvalidChangeRequest>(Some("create issue".to_string()), |d| {
@@ -279,7 +285,7 @@ fn initial_doc(author: Urn, title: String, description: String) -> History {
         .unwrap();
     let mut backend = automerge::Backend::new();
     let (_, change) = backend.apply_local_change(change.unwrap()).unwrap();
-    History::Automerge(change.raw_bytes().to_vec())
+    librad::collaborative_objects::EntryContents::Automerge(change.raw_bytes().to_vec())
 }
 
 #[derive(serde::Deserialize)]
@@ -299,15 +305,30 @@ pub struct Comment {
 impl TryFrom<&History> for Issue {
     type Error = anyhow::Error;
 
-    fn try_from(value: &History) -> Result<Self, Self::Error> {
-        match value {
-            History::Automerge(bytes) => {
-                let backend = automerge::Backend::load(bytes.clone())?;
-                let mut frontend = automerge::Frontend::new();
-                let patch = backend.get_patch()?;
-                frontend.apply_patch(patch)?;
-                serde_json::from_value(frontend.state().to_json()).map_err(|e| e.into())
+    fn try_from(history: &History) -> Result<Self, Self::Error> {
+        let (mut frontend, _backend) = evaluate_history(history)?;
+        serde_json::from_value(frontend.state().to_json()).map_err(|e| e.into())
+    }
+}
+
+fn evaluate_history(history: &librad::collaborative_objects::History) -> anyhow::Result<(automerge::Frontend, automerge::Backend)> {
+    let backend = history.traverse(automerge::Backend::new(), |mut backend, entry| {
+        eprintln!("loading entry");
+        match entry.contents() {
+            librad::collaborative_objects::EntryContents::Automerge(data) => {
+                match automerge::Change::from_bytes(data.clone()) {
+                    Ok(c) => {
+                        backend.apply_changes(vec![c]).ok();
+                    },
+                    Err(e) => {
+                        eprintln!("Error loading a change: {}", e);
+                    }
+                }
             }
         }
-    }
+        std::ops::ControlFlow::Continue(backend)
+    });
+    let mut frontend = automerge::Frontend::new();
+    frontend.apply_patch(backend.get_patch()?)?;
+    Ok((frontend, backend))
 }
